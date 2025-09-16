79389b19a3a6326a3bcfeec9db43aeb51d807726
EricTM code <93444335+EricTMcode@users.noreply.github.com>
2025-09-16 14:20:52 +0200

    Initial Commit

---
+++ UpgradeYourMac/UpgradeYourMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpgradeYourMacApp.swift
+//  UpgradeYourMac
+//
+//  Created by Eric on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct UpgradeYourMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
